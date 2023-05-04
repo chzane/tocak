83c09cc83d52fd2c587bfbd2f16ce9a3ccc43ecf
zane <112529607+chzane@users.noreply.github.com>
2023-05-04 21:30:16 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # tocak
-### A lightweight web server
+A lightweight web server
+
+![Github stars](https://img.shields.io/github/stars/chzane/tocak.svg?style=for-the-badge)
+![Team](https://img.shields.io/badge/TEAM-Solariix-blue?style=for-the-badge)
+![Team](https://img.shields.io/badge/License-MIT-blue?style=social)
+***
+**_The project continues to update, some functions may not be perfect, has a better suggestion please submit issues_**
+***
+Hi, thank you for using tocak as your web application server system.
+
+Tocak is a lightweight web application server system, it can help you quickly on your web applications.
+## install
+This project uses python3 or python2. Go check them out if you don't have them locally installed.
+```shell
+git clone https://github.com/chzane/tocak.git
+```
+## Usage
+Open the `web.config.tocak` file, this is the web application configuration file, you can change here application port, set up the index file, set the error page, etc.
+
+It's format should be like this
+
+```json
+{
+  "http_config": {
+    "index": "./webapp/",
+    "port": "8888"
+  },
+  "error_page": {
+    "404": " <title>404 error</title><h1>404 error</h1><hr><p>sorry,file not found.<br>Please check the URL you entered or contact developers.</p><p>tocak</p>"
+    ... ...
+  }
+}
+```
+
+`http_config` used to set the basic operating parameters.
+
+`error_page` used to set the error page.
+
+### http_config
+`index` : Set the directory of web app store, the default is `webapp`, we do not recommend changes.
+
+`port` : Set web application port.
+
+### error_page
+`ERRORCODE` : like `404`,`502`,etc.After setting error page.
+
+## License
+![Team](https://img.shields.io/badge/License-MIT-blue?style=social) @chzane
--- server.py
+++ server.py
@@ -2,63 +2,88 @@
 from socket import *
 import re
 import time
+import json
 
+try:
+    file = open("./web.config.tocak", "rb")
+    conf = json.loads(file.read())
+    file.close()
+except:
+    print("Server run error: 'web.config.tocak not find")
 
 def handle_client(client_socket):
-    recv_data = client_socket.recv(2048).decode("utf-8")
-    request_header_lines = recv_data.splitlines()
-    print("Detailed log")
-    for line in request_header_lines:
-        print(line)
+    request = client_socket.recv(20480).decode("utf-8")
+    request_header_lines = request.splitlines()
 
-    print("Brief information")
-    print("Access time ---->", time.asctime())
-    print("Request way ---->", request_header_lines[0])
-    print("file_path ---->", "./webapp/" + re.match(r"[^/]+/([^\s]*)", request_header_lines[0]).group(1))
-    ret = re.match(r"[^/]+/([^\s]*)", request_header_lines[0])
-    if ret:
-        file_path = "./webapp/" + ret.group(1)
-        retarray = [""]
-        i = 0
-        for word in ret.group(1):
-            retarray.append(word)
-            i = i + 1
-        if len(retarray[len(ret.group(1))]) == 0:
-            retarray[0] = "/"
-        if retarray[len(ret.group(1))] == "/":
-            file_path = "./webapp/"+ret.group(1)+"index.html"
-        if not(retarray[len(ret.group(1))] == "/") and not("index.html" in ret.group(1)):
-            file_path = "./webapp/" + ret.group(1) + "/index.html"
-        print("file_path *******", file_path)
-    print("log end=========================")
+    try:
+        print("Detailed log")
+
+        for line in request_header_lines:
+            print(line)
+        print("Brief information")
+        print("Access time ---->", time.asctime())
+        print("Request way ---->", request_header_lines[0])
+        print("file_path ---->", conf["http_config"]["index"] + re.match(r"[^/]+/([^\s]*)", request_header_lines[0]).group(1))
+
+        ret = re.match(r"[^/]+/([^\s]*)", request_header_lines[0])
+        if ret:
+            file_path = conf["http_config"]["index"] + ret.group(1)
+            retarray = [""]
+
+            i = 0
+            for word in ret.group(1):
+                retarray.append(word)
+                i = i + 1
+
+            if len(retarray[len(ret.group(1))]) == 0:
+                retarray[0] = "/"
+            if retarray[len(ret.group(1))] == "/":
+                file_path = conf["http_config"]["index"] + ret.group(1) + "index.html"
+            if not(retarray[len(ret.group(1))] == "/") and not("index.html" in ret.group(1)):
+                file_path = conf["http_config"]["index"] + ret.group(1) + "/index.html"
+
+            print("file_path *******", file_path)
+
+        print("log end=========================")
+    except:
+        print("=========================\nlog ERROR\n=========================")
 
     try:
         response_headers = "HTTP/1.1 200 OK\r\n"
         response_headers += "\r\n"
+
         file_name = file_path
-        f = open(file_name, "rb")
-        response_body = f.read()
-        f.close()
+
+        file = open(file_name, "rb")
+        response_body = file.read()
+        file.close()
 
         client_socket.send(response_headers.encode("utf-8"))
         client_socket.send(response_body)
     except:
         response_headers = "HTTP/1.1 404 not found\r\n"
         response_headers += "\r\n"
-        response_body = "<title>404 error</title><h1>404 error</h1><hr><p>sorry,file not found.<br>Please check the URL you entered or contact developers.</p><p>tocak</p>"
+
+        response_body = conf["error_page"]["404"]
+
         response = response_headers + response_body
         client_socket.send(response.encode("utf-8"))
 
     client_socket.close()
 
-
 def main():
-    server_socket = socket(AF_INET, SOCK_STREAM)
-    server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    server_socket.bind(('', 8888))
-    server_socket.listen(128)
-    print('Server run in "ip:7788"')
-    print('log on:')
+    try:
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        server_socket.bind(('', int(conf["http_config"]["port"])))
+        server_socket.listen(128)
+
+        print("Server run in 'ip:",conf["http_config"]["port"],"'")
+        print("log on:")
+    except:
+        print("Server run error")
+        exit()
+
     while True:
         client_socket, clientAddr = server_socket.accept()
         handle_client(client_socket)
